00d8c700e0038d7ff9bcd2af6c1046a414c8ac1f
bhavdip111 <bb.pambhar@gmail.com>
2023-02-28 17:59:15 +0530

    feat:resolve error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { InputRef } from "./components/InputRef";
+// import { InputRef } from "./components/InputRef";
 
 function App() {
   return (
-    <div className="flex justify-center max-w-screen-2xl  flex-col items-center bg-red-500">
-      <InputRef />
+    <div className="flex justify-center max-w-screen-2xl  flex-col items-center">
+      {/* <InputRef /> */}
     </div>
   );
 }
