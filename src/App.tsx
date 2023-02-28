a2e70e824eb6089a6292a27f2437e6af36b68b09
bhavdip111 <bb.pambhar@gmail.com>
2023-02-28 16:39:17 +0530

    feat:add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { InputRef } from "./components/InputRef";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center max-w-screen-2xl  flex-col items-center bg-red-500">
+      <InputRef />
     </div>
   );
 }
