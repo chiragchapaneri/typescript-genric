9ec8090f334c23d762da89fcc8b27b71ea6c24c1
chiragchapaneri <chapanerichirag500@gmail.com>
2023-03-22 11:15:26 +0530

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { InputRef } from "./components/InputRef";
 
 function App() {
   return (
-    <div className="flex justify-center max-w-screen-2xl  flex-col items-center bg-red-500">
-      <InputRef />
-    </div>
+    <div className="flex justify-center max-w-screen-2xl  flex-col items-center bg-red-500"></div>
   );
 }
 
